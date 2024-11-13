a884941f962f1710ca2d850e227bac244eb44506
MiroKilpinen <miro.kilpinen@edu.salpaus.fi>
2024-11-13 13:18:30 +0200

    register page finished for now, won't actually register since no backend

--- Client/src/pages/LoginPage.tsx
+++ Client/src/pages/LoginPage.tsx
@@ -6,6 +6,7 @@ import { Button, Field, Input } from '@fluentui/react-components';
 function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [pageState, setPageState] = useState('');
   const navigate = useNavigate();
 
   // Change this logic once server is implemented
@@ -14,23 +15,58 @@ function LoginPage() {
     navigate('/');
   }
 
-  return (
-    <div className='login-holder'>
-      <div className='login-card'>
-        <div className='title'>Buildscope</div>
+  function changePage() {
+    if (pageState === '') {
+      setPageState("register");
+    } else {
+      setPageState('');
+    }
+  }
 
-        <Field>
-            <Input type='text' onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
-        </Field>
+  function register() {
+    // Temporary, not actually registering
+    setPageState('');
+  }
 
-        <Field>
-            <Input type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
-        </Field>
+  if (pageState === '') {
+    return (
+      <div className='login-holder'>
+        <div className='login-card'>
+          <div className='title'>Buildscope</div>
 
-        <Button onClick={logIn}>Log in</Button>
-      </div>  
-    </div>
-  );
+          <Field>
+              <Input type='text' onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+          </Field>
+
+          <Field>
+              <Input type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+          </Field>
+
+          <Button onClick={logIn}>Log in</Button>
+          <Button onClick={changePage}>Register</Button>
+        </div>  
+      </div>
+    );
+  } else {
+    return (
+      <div className='login-holder'>
+        <div className='login-card'>
+          <div className='title'>Buildscope</div>
+
+          <Field>
+              <Input type='text' onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+          </Field>
+
+          <Field>
+              <Input type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+          </Field>
+
+          <Button onClick={register}>Register</Button>
+          <Button onClick={changePage}>Back</Button>
+        </div>  
+      </div>
+    );
+  }
 }
 
 export default LoginPage;
