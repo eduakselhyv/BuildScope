8dde156103304bff5cec0bd9e67382fa6180bf02
MiroKilpinen <miro.kilpinen@edu.salpaus.fi>
2024-11-13 13:20:03 +0200

    formatting

--- Client/src/pages/LoginPage.tsx
+++ Client/src/pages/LoginPage.tsx
@@ -1,69 +1,85 @@
-import { useState } from 'react';
-import '../assets/styles/login.css';
-import { useNavigate } from 'react-router-dom';
-import { Button, Field, Input } from '@fluentui/react-components';
+import { useState } from "react";
+import "../assets/styles/login.css";
+import { useNavigate } from "react-router-dom";
+import { Button, Field, Input } from "@fluentui/react-components";
 
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [pageState, setPageState] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [pageState, setPageState] = useState("");
   const navigate = useNavigate();
 
   // Change this logic once server is implemented
   function logIn() {
-    localStorage.setItem('user', username);
-    navigate('/');
+    localStorage.setItem("user", username);
+    navigate("/");
   }
 
   function changePage() {
-    if (pageState === '') {
+    if (pageState === "") {
       setPageState("register");
     } else {
-      setPageState('');
+      setPageState("");
     }
   }
 
   function register() {
     // Temporary, not actually registering
-    setPageState('');
+    setPageState("");
   }
 
-  if (pageState === '') {
+  if (pageState === "") {
     return (
-      <div className='login-holder'>
-        <div className='login-card'>
-          <div className='title'>Buildscope</div>
+      <div className="login-holder">
+        <div className="login-card">
+          <div className="title">Buildscope</div>
 
           <Field>
-              <Input type='text' onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+            <Input
+              type="text"
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+            />
           </Field>
 
           <Field>
-              <Input type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+            <Input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
           </Field>
 
           <Button onClick={logIn}>Log in</Button>
           <Button onClick={changePage}>Register</Button>
-        </div>  
+        </div>
       </div>
     );
   } else {
     return (
-      <div className='login-holder'>
-        <div className='login-card'>
-          <div className='title'>Buildscope</div>
+      <div className="login-holder">
+        <div className="login-card">
+          <div className="title">Buildscope</div>
 
           <Field>
-              <Input type='text' onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+            <Input
+              type="text"
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+            />
           </Field>
 
           <Field>
-              <Input type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+            <Input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
           </Field>
 
           <Button onClick={register}>Register</Button>
           <Button onClick={changePage}>Back</Button>
-        </div>  
+        </div>
       </div>
     );
   }
