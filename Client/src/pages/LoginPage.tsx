a8031a566252dc96774e503a19f323e0538b04e3
eduakselhyv <akseli.hyvonen@edu.salpaus.fi>
2024-11-14 18:46:45 +0200

    Improved css of login page, added register button and worked on tasks page

--- Client/src/pages/LoginPage.tsx
+++ Client/src/pages/LoginPage.tsx
@@ -1,29 +1,41 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Field, Input } from '@fluentui/react-components';
-import { makeStyles, shorthands } from '@griffel/react';
+import { makeStyles } from '@griffel/react';
 
 const useStyles = makeStyles({
     loginHolder: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        backgroundColor: 'lightgray',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      backgroundColor: 'lightgray',
     },
     loginCard: {
-        backgroundColor: 'white',
-        width: '200px',
-        padding: '110px 20px 85px',
-        borderRadius: '10px',
-        boxShadow: '-5px 5px 10px rgba(0, 0, 0, 0.2), 5px 5px 10px rgba(0, 0, 0, 0.2)',
-        ...shorthands.padding('110px', '20px', '85px'),
+      backgroundColor: 'white',
+      borderRadius: '10px',
+      boxShadow: '-5px 5px 10px rgba(0, 0, 0, 0.2), 5px 5px 10px rgba(0, 0, 0, 0.2)',
+      padding: '110px 20px 80px',
+      width: '300px',
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      alignItems: 'center'
     },
     title: {
-        textAlign: 'center',
-        fontSize: '30px',
-        paddingBottom: '20px',
+      textAlign: 'center',
+      fontSize: '30px',
+      paddingBottom: '20px',
     },
+    inputCustom: {
+      '& .fui-Input__input': {
+        background: 'rgb(0, 0, 0, 0.1)',
+        paddingLeft: '10px'
+      }
+    },
+    buttonHolder: {
+      marginTop: '40px'
+    }
 });
 
 function LoginPage() {
@@ -38,20 +50,34 @@ function LoginPage() {
     navigate('/');
   }
 
+  function register() {
+    
+    // Logic to register user
+
+    /*
+    if (response.IsSuccessStatusCode) {
+      logIn();
+    }
+    */
+  }
+
   return (
     <div className={styles.loginHolder}>
       <div className={styles.loginCard}>
         <div className={styles.title}>Buildscope</div>
 
         <Field>
-            <Input type='text' onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+            <Input className={styles.inputCustom} type='text' onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
         </Field>
 
         <Field>
-            <Input type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+            <Input className={styles.inputCustom} type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
         </Field>
 
-        <Button onClick={logIn}>Log in</Button>
+        <div className={styles.buttonHolder}>
+          <Button onClick={logIn}>Log in</Button>
+          <Button onClick={register}>Register</Button>
+        </div>
       </div>  
     </div>
   );
